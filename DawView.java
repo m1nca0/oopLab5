d0e4e5817fcef4f1e66a8082534f910ce42ffb7d
Mihail VASILEV <noname890256@gmail.com>
2026-03-12 13:36:08 +0800

    remade view, add popup menu

--- DawView.java
+++ DawView.java
@@ -4,66 +4,40 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DawView extends JFrame {
-    private JTextField textField11;
-    private JTextField textField12;
     private Daw daw;
     private Kick kick;
     private Snare snare;
     private Hat hat;
+    private MyTable myTableModel;
+    private JComboBox<JButton> addSample;
 
     public DawView() {
-        JLabel label1 = new JLabel("Добавить Сэмпл");
-        JLabel label11 = new JLabel("Имя");
-        JLabel label12 = new JLabel("Группа");
-        textField11 = new JTextField("");
-        textField12 = new JTextField("");
-        JButton button1 = new JButton("Добавить");
-        JTable table;
-        JScrollPane scrollPane;
         Daw daw = new Daw();
-        MyTable myTableModel;
-        
+        myTableModel = new MyTable(daw);
+        JTable table = new JTable(myTableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+        JButton addButton = new JButton("Добавить");
 
-        setSize(1400, 500);
+        setSize(700, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
 
-        Container p1 = new Container(); //контейнер для левой части окна
-        add(p1);
-        p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false);
+        toolBar.add(addButton);
+        add(toolBar, BorderLayout.NORTH);
 
+        add(scrollPane, BorderLayout.CENTER);
 
-        p1.add(label1);
-        p1.add(label11);
-        textField11.setSize(80,30);
-        p1.add(textField11);
-        p1.add(label12);
-        textField12.setSize(80,30);
-        p1.add(textField12);
-        p1.add(button1);
-
-
-        Container p2=new Container();//контейнер для правой части окна
-        p2.setLayout(new FlowLayout());
-        add(p2);
-
-
-        table=new JTable();
-        table.setModel(myTableModel=new MyTable(daw));
-        
-        table.setAutoCreateRowSorter(true);
-        scrollPane = new JScrollPane(table);
-
-        scrollPane.setPreferredSize(new Dimension(1000, 500));
-        p2.add(scrollPane);
+        addButton.addActionListener(e -> showAddMenu(addButton));
         setVisible(true);
+    }
+    private void showAddMenu(JButton button)
+    {
+        JPopupMenu menu = new JPopupMenu();
 
-        // button1.addActionListener(new ActionListener() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {
-        //         student=new Student(textField11.getText(),textField12.getText());
-        //         myTableModel.addStudent(student);
-        //     }
-        // });
+        JMenuItem kickItem = new JMenuItem("Добавить KICK");
+        menu.add(kickItem);
+        menu.show(button, 0, button.getHeight());
     }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import javax.swing.text.View;
 
 public class Main{
     public static void main(String[] args) {
-        DawView view= new DawView();
+        DawView view = new DawView();
     }
 }
