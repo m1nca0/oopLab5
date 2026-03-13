606ef57ceec56965d9a9abbc33f90ee673af4324
Mihail VASILEV <noname890256@gmail.com>
2026-03-13 09:26:08 +0800

    finish addition logic

--- DawView.java
+++ DawView.java
@@ -41,10 +41,10 @@ public class DawView extends JFrame {
         kickItem.addActionListener(e -> showKickDialog());
 
         JMenuItem snareItem = new JMenuItem("Добавить SNARE");
-        // snareItem.addActionListener(e -> showSnareDialog());
+        snareItem.addActionListener(e -> showSnareDialog());
 
         JMenuItem hatItem = new JMenuItem("Добавить HAT");
-        // hatItem.addActionListener(e -> showHatDialog());
+        hatItem.addActionListener(e -> showHatDialog());
 
 
         menu.add(kickItem);
@@ -102,6 +102,122 @@ public class DawView extends JFrame {
             Kick kick = new Kick(name, len, volume, lowFrequency, highFrequency, bassLevel);
             myTableModel.addSamples(kick);
             JOptionPane.showMessageDialog(this, "Kick - " + name + " успешно добавлен!");
+        }
+    }
+    private void showSnareDialog(){
+        JTextField nameSnare = new JTextField(15);
+        JTextField lenSnare = new JTextField(15);
+        JTextField volumeSnare = new JTextField(15);
+        JTextField lowFrequencySnare = new JTextField(15);
+        JTextField highFrequencySnare = new JTextField(15);
+        JTextField resonanceSnare = new JTextField(15);
+        JTextField punchSnare = new JTextField(15);
+
+        JPanel panel = new JPanel(new GridLayout(7, 2, 5, 5));
+        panel.add(new JLabel("Название: "));
+        panel.add(nameSnare);
+        panel.add(new JLabel("Длина (в милисекундах, 0-10000): "));
+        panel.add(lenSnare);
+        panel.add(new JLabel("Громкость: "));
+        panel.add(volumeSnare);
+        panel.add(new JLabel("Низ: "));
+        panel.add(lowFrequencySnare);
+        panel.add(new JLabel("Вверх: "));
+        panel.add(highFrequencySnare);
+        panel.add(new JLabel("Резонанс: "));
+        panel.add(resonanceSnare);
+        panel.add(new JLabel("Удар: "));
+        panel.add(punchSnare);        
+
+        int result = JOptionPane.showConfirmDialog(
+            this,
+            panel,
+            "Добавление snare'а",
+            JOptionPane.OK_CANCEL_OPTION,
+            JOptionPane.PLAIN_MESSAGE
+        );
+        if (result == JOptionPane.OK_OPTION) {
+            if (nameSnare.getText().trim().isEmpty() ||
+                lenSnare.getText().trim().isEmpty() ||
+                volumeSnare.getText().trim().isEmpty() ||
+                lowFrequencySnare.getText().trim().isEmpty() ||
+                highFrequencySnare.getText().trim().isEmpty() ||
+                resonanceSnare.getText().trim().isEmpty() ||
+                punchSnare.getText().trim().isEmpty()) {
+                
+                JOptionPane.showMessageDialog(this, "Для добавления сэмпла заполните все поля!");
+
+            }
+            String name = nameSnare.getText().trim();
+            int len = Integer.parseInt(lenSnare.getText().trim());
+            double volume = Double.parseDouble(volumeSnare.getText().trim());
+            int lowFrequency = Integer.parseInt(lowFrequencySnare.getText().trim());
+            int highFrequency = Integer.parseInt(highFrequencySnare.getText().trim());
+            int resonance = Integer.parseInt(resonanceSnare.getText().trim());
+            int punch = Integer.parseInt(punchSnare.getText().trim());
+
+            Snare snare = new Snare(name, len, volume, lowFrequency, highFrequency, resonance, punch);
+            myTableModel.addSamples(snare);
+            JOptionPane.showMessageDialog(this, "Snare - " + name + " успешно добавлен!");
+        }
+    }
+
+
+    private void showHatDialog(){
+        JTextField nameHat = new JTextField(15);
+        JTextField lenHat = new JTextField(15);
+        JTextField volumeHat = new JTextField(15);
+        JTextField lowFrequencyHat = new JTextField(15);
+        JTextField highFrequencyHat = new JTextField(15);
+        JTextField tailLengthHat = new JTextField(15);
+        JTextField closedHat = new JTextField(15);
+
+        JPanel panel = new JPanel(new GridLayout(7, 2, 5, 5));
+        panel.add(new JLabel("Название: "));
+        panel.add(nameHat);
+        panel.add(new JLabel("Длина (в милисекундах, 0-10000): "));
+        panel.add(lenHat);
+        panel.add(new JLabel("Громкость: "));
+        panel.add(volumeHat);
+        panel.add(new JLabel("Низ: "));
+        panel.add(lowFrequencyHat);
+        panel.add(new JLabel("Вверх: "));
+        panel.add(highFrequencyHat);
+        panel.add(new JLabel("Длина хвоста: "));
+        panel.add(tailLengthHat);
+        panel.add(new JLabel("Открытый/Закрытый: "));
+        panel.add(closedHat);
+
+        int result = JOptionPane.showConfirmDialog(
+            this,
+            panel,
+            "Добавление Hat'а",
+            JOptionPane.OK_CANCEL_OPTION,
+            JOptionPane.PLAIN_MESSAGE
+        );
+        if (result == JOptionPane.OK_OPTION) {
+            if (nameHat.getText().trim().isEmpty() ||
+                lenHat.getText().trim().isEmpty() ||
+                volumeHat.getText().trim().isEmpty() ||
+                lowFrequencyHat.getText().trim().isEmpty() ||
+                highFrequencyHat.getText().trim().isEmpty() ||
+                tailLengthHat.getText().trim().isEmpty() ||
+                closedHat.getText().trim().isEmpty()) {
+                
+                JOptionPane.showMessageDialog(this, "Для добавления сэмпла заполните все поля!");
+
+            }
+            String name = nameHat.getText().trim();
+            int len = Integer.parseInt(lenHat.getText().trim());
+            double volume = Double.parseDouble(volumeHat.getText().trim());
+            int lowFrequency = Integer.parseInt(lowFrequencyHat.getText().trim());
+            int highFrequency = Integer.parseInt(highFrequencyHat.getText().trim());
+            int tailLength = Integer.parseInt(tailLengthHat.getText().trim());
+            boolean closed = tailLengthHat.getText().trim() == "Закрытый" ? false : true;
+
+            Hat hat = new Hat(name, len, volume, lowFrequency, highFrequency, tailLength, closed);
+            myTableModel.addSamples(hat);
+            JOptionPane.showMessageDialog(this, "Hat - " + name + " успешно добавлен!");
         }
     }
 }
